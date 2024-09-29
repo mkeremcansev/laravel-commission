fa93af557b64395992d0a2bf197d487629b72474
Mustafa Kerem CANSEV <76810832+mkeremcansev@users.noreply.github.com>
2024-09-29 20:56:11 +0300

    main: create `FixedCommissionCalculatorContext` class for fixed calculator context

---
+++ src/Services/Factory/Calculator/Contexts/FixedCommissionCalculatorContext.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Mkeremcansev\LaravelCommission\Services\Factory\Calculator\Contexts;
+
+use Illuminate\Database\Eloquent\Model;
+use Mkeremcansev\LaravelCommission\Enums\CommissionCalculateHistoryReasonEnum;
+use Mkeremcansev\LaravelCommission\Enums\CommissionCalculateHistoryStatusEnum;
+use Mkeremcansev\LaravelCommission\Models\Commission;
+
+class FixedCommissionCalculatorContext extends BaseCommissionCalculatorContext
+{
+    public function __construct(
+        public Commission $commission,
+        public Model $model,
+        public int $originalAmount,
+        public int $commissionAmount,
+        public int $totalAmount,
+        public CommissionCalculateHistoryStatusEnum $status,
+        public CommissionCalculateHistoryReasonEnum $reason,
+        public string $groupId,
+        public string $column
+    )
+    {
+    }
+}

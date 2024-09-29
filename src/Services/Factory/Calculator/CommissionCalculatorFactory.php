d50d444ba514cfff66ba9b131d78fa6e9fe09abf
Mustafa Kerem CANSEV <76810832+mkeremcansev@users.noreply.github.com>
2024-09-29 20:57:53 +0300

    main: create `CommissionCalculatorFactory` factory machine for resolving commission type calculator

---
+++ src/Services/Factory/Calculator/CommissionCalculatorFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Mkeremcansev\LaravelCommission\Services\Factory\Calculator;
+
+use Exception;
+use Illuminate\Database\Eloquent\Model;
+use Mkeremcansev\LaravelCommission\Enums\CommissionTypeEnum;
+use Mkeremcansev\LaravelCommission\Models\Commission;
+use Mkeremcansev\LaravelCommission\Services\Factory\Calculator\Calculators\FixedCommissionCalculator;
+use Mkeremcansev\LaravelCommission\Services\Factory\Calculator\Calculators\PercentageCommissionCalculator;
+use Mkeremcansev\LaravelCommission\Services\Factory\Calculator\Contracts\CommissionCalculatorInterface;
+
+class CommissionCalculatorFactory
+{
+    /**
+     * @throws Exception
+     */
+    public static function make(Commission $commission, Model $model): CommissionCalculatorInterface
+    {
+        return match ($commission->type) {
+            CommissionTypeEnum::FIXED => new FixedCommissionCalculator($commission, $model),
+            CommissionTypeEnum::PERCENTAGE => new PercentageCommissionCalculator($commission, $model),
+            default => throw new Exception("Invalid commission type"),
+        };
+    }
+}

b8f7615fea79bbd9ba81844da91c3f6c93ddc571
Mustafa Kerem CANSEV <76810832+mkeremcansev@users.noreply.github.com>
2024-09-29 20:52:50 +0300

    main: create `CommissionCalculateHistory` model with migration, factory

---
+++ database/migrations/2024_09_29_003356_create_commission_calculate_histories_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('commission_calculate_histories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(\Mkeremcansev\LaravelCommission\Models\Commission::class);
+            $table->morphs('model');
+            $table->uuid('group_id');
+            $table->string('column');
+            $table->unsignedBigInteger('original_amount');
+            $table->unsignedBigInteger('calculated_amount');
+            $table->unsignedBigInteger('commission_amount');
+            $table->tinyInteger('status');
+            $table->tinyInteger('reason')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('commission_calculate_histories');
+    }
+};
---
+++ src/Models/CommissionCalculateHistory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Mkeremcansev\LaravelCommission\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Mkeremcansev\LaravelCommission\Enums\CommissionCalculateHistoryReasonEnum;
+use Mkeremcansev\LaravelCommission\Enums\CommissionCalculateHistoryStatusEnum;
+
+class CommissionCalculateHistory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'commission_id',
+        'model_id',
+        'model_type',
+        'group_id',
+        'column',
+        'original_amount',
+        'calculated_amount',
+        'commission_amount',
+        'status',
+        'reason',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'status' => CommissionCalculateHistoryStatusEnum::class,
+            'reason' => CommissionCalculateHistoryReasonEnum::class,
+        ];
+    }
+}

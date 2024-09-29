ef1528e9dbd3bc0fb44f237f17684600150f00d0
Mustafa Kerem CANSEV <76810832+mkeremcansev@users.noreply.github.com>
2024-09-29 20:43:47 +0300

    main: update `LaravelCommissionServiceProvider` for new package struct

---
+++ src/LaravelCommissionServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Mkeremcansev\LaravelCommission;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Mkeremcansev\LaravelCommission\Commands\LaravelCommissionCommand;
+
+class LaravelCommissionServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name('laravel-commission')
+            ->hasConfigFile();
+    }
+
+    public function boot(): void
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        $this->publishes([
+            __DIR__.'/../database/migrations' => database_path('migrations'),
+        ], 'laravel-commission-migrations');
+
+        $this->publishes([
+            __DIR__.'/../config/laravel-commission.php' => config_path('laravel-commission.php'),
+        ], 'laravel-commission-config');
+    }
+}

e1c08c0a73ab9a8c5a962da978d767d6e8732304
Mustafa Kerem CANSEV <76810832+mkeremcansev@users.noreply.github.com>
2024-09-30 23:36:32 +0300

    main: add `Product` fixture on `Model` based

---
+++ tests/Fixtures/Models/Product.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Mkeremcansev\LaravelCommission\Tests\Fixtures\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Mkeremcansev\LaravelCommission\Contracts\HasCommissionInterface;
+use Mkeremcansev\LaravelCommission\Traits\HasCommission;
+
+class Product extends Model implements HasCommissionInterface
+{
+    use HasCommission;
+
+    public int $id = 1;
+    public int $amount = 100;
+
+    public function getCommissionableColumns(): array
+    {
+        return ['amount'];
+    }
+}

ef0879515393731d7ce1c1dbc2b89125188398c1
Mustafa Kerem CANSEV <76810832+mkeremcansev@users.noreply.github.com>
2024-09-30 21:19:27 +0300

    main: update `PercentageCommissionCalculator@calculate` for new `is_total` calculating

--- src/Services/Factory/Calculator/Calculators/PercentageCommissionCalculator.php
+++ src/Services/Factory/Calculator/Calculators/PercentageCommissionCalculator.php
@@ -29,11 +29,28 @@ class PercentageCommissionCalculator extends BaseCalculator implements Commissio
                 'group_id' => $this->model->group_id,
             ])->sum('commission_amount');
 
+            /**
+             * Here we calculate the total amount that can be calculated
+             * The total commission amount by history + the current amount
+             */
             $totalCalculableAmount = $totalCommissionAmountByHistory + $amount;
-            $commissionAmountWithoutHistoryAmount = $totalCalculableAmount * ($this->commission->rate / 100);
-            $commissionAmount = $commissionAmountWithoutHistoryAmount;
-            $totalAmount = $amount + $commissionAmountWithoutHistoryAmount + $totalCommissionAmountByHistory;
+
+            /**
+             * Here we calculate the total commission amount
+             * The total amount that can be calculated * the rate of the commission
+             */
+            $totalCommissionAmount = $totalCalculableAmount * ($this->commission->rate / 100);
+
+            $commissionAmount = $totalCommissionAmount;
+
+            /**
+             * Here we calculate the total amount
+             * The total amount + the total commission amount by history + the total commission amount
+             */
+            $totalAmount = $amount + $totalCommissionAmount + $totalCommissionAmountByHistory;
         }
+
+        $totalAmount = $this->round($totalAmount);
 
         $context = match ($status) {
             CommissionCalculateHistoryStatusEnum::FAILED => new PercentageCommissionCalculatorContext(

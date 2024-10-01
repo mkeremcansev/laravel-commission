54865b4a7124dc125f0d9bab378ddd5215a66642
mkeremcansev <mkeremcansev@users.noreply.github.com>
2024-10-01 21:08:23 +0000

    Fix styling

--- src/Services/Contexts/CommissionBundleContext.php
+++ src/Services/Contexts/CommissionBundleContext.php
@@ -4,8 +4,7 @@ namespace Mkeremcansev\LaravelCommission\Services\Contexts;
 
 use Mkeremcansev\LaravelCommission\Models\Commission;
 
-class CommissionBundleContext {
-    public function __construct(public Commission $commission, public string $column)
-    {
-    }
+class CommissionBundleContext
+{
+    public function __construct(public Commission $commission, public string $column) {}
 }

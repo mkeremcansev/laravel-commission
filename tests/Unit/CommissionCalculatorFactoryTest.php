973dcd99be3a757d5a245d561a468b126b3f0841
Mustafa Kerem CANSEV <76810832+mkeremcansev@users.noreply.github.com>
2024-10-03 20:24:05 +0300

    main: create `CommissionCalculatorFactoryTest` with test methods

--- tests/Unit/CommissionCalculatorFactoryTest.php
+++ tests/Unit/CommissionCalculatorFactoryTest.php
@@ -1,24 +1,28 @@
 <?php
 
 use Mkeremcansev\LaravelCommission\Models\Commission;
-use Mkeremcansev\LaravelCommission\Services\Calculators\FixedCommissionCalculator;
-use Mkeremcansev\LaravelCommission\Services\Calculators\PercentageCommissionCalculator;
 use Mkeremcansev\LaravelCommission\Services\CommissionCalculatorFactory;
+use Mkeremcansev\LaravelCommission\Services\Contexts\CommissionBundleContext;
 use Mkeremcansev\LaravelCommission\Tests\Fixtures\Models\Product;
 
 describe('make()', function () {
     it('can return fixed commission calculator instance', function () {
         // Arrange:
-        $model = new Product;
-
         $commission = Commission::factory()
             ->withFixedCommission()
             ->create();
 
+        $model = new Product;
+        $commissionGroupId = Str::uuid()->toString();
+        $bundleContext = new CommissionBundleContext($commission, 'amount', $commissionGroupId);
+
         // Act & Assert:
-        expect(CommissionCalculatorFactory::make($commission, $model))
-            ->toBeInstanceOf(FixedCommissionCalculator::class)
-            ->commission->id
+        expect(CommissionCalculatorFactory::make($bundleContext, $model))
+            ->context
+            ->toBeInstanceOf(CommissionBundleContext::class)
+            ->context->commission
+            ->toBeInstanceOf(Commission::class)
+            ->context->commission->id
             ->toBe($commission->id)
             ->model->id->toBe($model->id)
             ->model->getCommissionableColumns()
@@ -27,16 +31,21 @@ describe('make()', function () {
 
     it('can return percentage commission calculator instance', function () {
         // Arrange:
-        $model = new Product;
-
         $commission = Commission::factory()
             ->withPercentageCommission()
             ->create();
 
+        $model = new Product;
+        $commissionGroupId = Str::uuid()->toString();
+        $bundleContext = new CommissionBundleContext($commission, 'amount', $commissionGroupId);
+
         // Act & Assert:
-        expect(CommissionCalculatorFactory::make($commission, $model))
-            ->toBeInstanceOf(PercentageCommissionCalculator::class)
-            ->commission->id
+        expect(CommissionCalculatorFactory::make($bundleContext, $model))
+            ->context
+            ->toBeInstanceOf(CommissionBundleContext::class)
+            ->context->commission
+            ->toBeInstanceOf(Commission::class)
+            ->context->commission->id
             ->toBe($commission->id)
             ->model->id->toBe($model->id)
             ->model->getCommissionableColumns()
